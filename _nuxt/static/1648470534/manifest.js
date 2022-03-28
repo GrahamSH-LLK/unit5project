01165e6d898401126fcb7fec70fb04a699a42e27
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2022-03-28 14:59:51 +0000

    deploy: 7ff9caf9a9c2e91c55b4d8e200c31f71bf74b53a

--- _nuxt/static/1648470534/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fbranches","\u002Fconstitution","\u002Ffederalism","\u002Flocalgov","\u002Ftaxes"]})
---
+++ _nuxt/static/1648479590/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Ffederalism","\u002Flocalgov","\u002Ftaxes","\u002Fbranches","\u002F","\u002Fconstitution"]})
